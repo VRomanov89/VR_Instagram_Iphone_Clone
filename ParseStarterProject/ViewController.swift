c0a3d05559e91c8650110cb5ace44e4f0c62d502
Vladimir Romanov <vladromweb@gmail.com>
2015-10-17 21:07:21 -0400

    Added layout and signup functionality.
    Added the final first page layout to the app and implemented
    username/password check functionality.

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -13,7 +13,20 @@ import Parse
 class ViewController: UIViewController{
     
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
-    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @available(iOS 8.0, *)
+    @IBAction func signUp(sender: AnyObject) {
+        if username.text == "" || password.text == "" {
+            let alert = UIAlertController(title: "Error in form", message: "Please enter a username and password", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: {(action) -> Void in
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+       @IBAction func Login(sender: AnyObject) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
